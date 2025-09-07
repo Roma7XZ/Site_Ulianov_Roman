002d59580a5a69befbbebd3c081ebe3871d23f46
Roma7XZ <romaulianov2007@gmail.com>
2025-09-07 20:49:05 +0300

    Theme dark/light

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -6,7 +6,7 @@ const codeImg = new URL('../assets/code.png', import.meta.url).href
 <template>
   <section class="about">
     <h1>Ульянов Роман</h1>
-    <p>Фронтенд‑разработчик. Делаю быстрые и удобные веб‑интерфейсы, Linux администратор, а также младший специалист по виртуализации.</p>
+    <p>Фронтенд‑разработчик. Делаю быстрые и удобные веб‑сайты, Linux администратор, а также младший специалист по виртуализации.</p>
     <div class="center">
       <a :href="resumeUrl" target="_blank" rel="noopener" class="btn">
         <img class="btn-icon" :src="codeImg" alt="" aria-hidden="true" />
